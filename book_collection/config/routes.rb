393810980548945df309e04ee6bfa621f044c9bc
Mark Costa <markbc25@tamu.edu>
2024-02-16 17:12:49 -0600

    CRUD implemented

--- book_collection/config/routes.rb
+++ book_collection/config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/new'
+  get 'books/_form'
+  get 'books/edit'
+  get 'books/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :books
+  root 'books#index'
+  
 end
